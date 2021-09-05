32246d00d69547360d3fc6c6d48f2656b289f789
Jillian Werner <jillianlwerner@gmail.com>
2021-09-05 19:24:47 -0400

    Reorganize src/ structure to separate SCSS and JS; split SCSS into partials

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "./style.scss";
-import "./template.html"; // For dev-server hot-reload
-
-console.log("JS is working!");
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import "../scss/style.scss";
+import "../template.html"; // For dev-server hot-reload
+
+console.log("JS is working!");
