228dd6de0009bdf1ac29a023df72290d25ca09d2
Jillian Werner <jillianlwerner@gmail.com>
2021-09-20 14:18:51 -0400

    Rewrite fetch function to async; move display outside of fetch function

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -3,27 +3,36 @@ import backupParks from "./backup.js";
 import { displayParkData, clearParksCards } from "./displayData.js";
 import states from "./formStates.js";
 
-const fetchParksData = (state) => {
-  return fetch(
-    `https://developer.nps.gov/api/v1/parks?stateCode=${state}&limit=12&api_key=${nationalParksKey}`,
-    { mode: "cors" }
-  );
-};
+const fetchParksData = async (state) => {
+  let parks;
 
-const parksData = async (state) => {
   try {
-    const response = await fetchParksData(state);
+    const response = await fetch(
+      `https://developer.nps.gov/api/v1/parks?stateCode=${state}&limit=12&api_key=${nationalParksKey}`,
+      { mode: "cors" }
+    );
     const data = await response.json();
 
-    displayParkData(data.data);
+    if (response.ok) {
+      parks = data.data;
+    } else {
+      parks = backupParks.data;
+    }
   } catch {
-    displayParkData(backupParks.data);
+    parks = backupParks.data;
   }
+
+  return parks;
+};
+
+const fetchAndDisplayData = async (state) => {
+  const parks = await fetchParksData(state);
+  displayParkData(parks);
 };
 
 const getStateFromDropdown = (formInput) => {
   clearParksCards();
-  parksData(formInput.value);
+  fetchAndDisplayData(formInput.value);
 };
 
 const getForm = () => document.getElementById("state-search");
@@ -41,10 +50,10 @@ const appendOptionsToForm = () => {
   for (let state in states) {
     let option = document.createElement("option");
     option.value = state;
-    option.name = state;
+    option.setAttribute("name", state);
     option.innerText = states[state];
     selectForm.appendChild(option);
   }
 };
 
-export { parksData, addListenerToForm, appendOptionsToForm };
+export { fetchAndDisplayData, addListenerToForm, appendOptionsToForm };
